5642123b6c66480c56c2d01c15baf1c5c7ae6ccd
carrotflakes <carrotflakes@gmail.com>
2018-10-04 22:47:42 +0900

    add show_policy_value.py

---
+++ show_policy_value.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from game import Game
+from agent_ai import AgentAI, game_to_board
+import sys
+
+sess = tf.Session()
+agent = AgentAI(sess)
+saver = tf.train.Saver()
+saver.restore(sess, 'model-{}'.format(sys.argv[1]))
+game = Game()
+[policy], [value] = sess.run([agent.policy_, agent.value_], {agent.board: [game_to_board(game)]})
+
+for y in range(8):
+    for x in range(8):
+        print('{:5f} '.format(policy[y, x]), end='')
+    print()
+
+print('value: {}'.format(value))
